2828653b38bb47f48fb95000ba23ca2a6e9712f5
Daniil <pozdeev.daniil@thc.games>
2025-07-01 20:40:39 +0400

    WIP Window

--- OpenGlApplication/OpenGlApplication/main.cpp
+++ OpenGlApplication/OpenGlApplication/main.cpp
@@ -2,17 +2,35 @@
 #include <glut.h>
 #include <iostream>
 
+static void RenderSceneCB();
+static void InitializeWindow(int weight, int height, int x, int y, const char* nameWindow);
 
 int main(int args, char* argv[])
 {
 	glutInit(&args, argv);
-	glutCreateWindow("GLEW Test");
-	GLenum err = glewInit();
-	if (GLEW_OK != err)
-	{
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-	}
-	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
-	getchar();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	InitializeWindow(1920, 1080, 100, 100, "First Window");
+
+	GLclampf Red = 0.0f, Green = 1.0f, Blue = 0.0f, Alpha = 0.0f;
+	glClearColor(Red, Green, Blue, Alpha);
+
+	glutDisplayFunc(RenderSceneCB);
+	glutMainLoop();
+
 	return 0;
+}
+
+static void RenderSceneCB()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutPostRedisplay();
+	glutSwapBuffers();
+}
+
+static void InitializeWindow(int weight, int height, int x, int y, const char* nameWindow)
+{
+	glutInitWindowSize(weight, height);
+	glutInitWindowPosition(x, y);
+	glutCreateWindow(nameWindow);
 }
